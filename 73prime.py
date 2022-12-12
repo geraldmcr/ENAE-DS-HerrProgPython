4093ebd41d25001887b73d007b4f4067f57b2f67
geraldmcr <gtoito@gmail.com>
2022-12-12 12:47:53 -0600

    Create 73prime.py

---
+++ 73prime.py
@@ -0,0 +1,20 @@
+def is_prime (y):
+  for x in range (2, y):
+    if y%x == 0:
+        result = False
+        break
+  if y == x + 1:
+    result = True
+  return result
+
+contador = 0
+last_prime = 1
+next_number = 2
+print(next_number)
+
+while contador<72:
+    next_number = next_number + 1
+    if is_prime (next_number):
+        contador = contador + 1
+        last_prime = next_number
+        print(last_prime)
